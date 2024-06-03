8d120cf8b9998f9283a8cf41b938c8b57b291c22
AliAhmed205 <Ali_lazuli@outlook.com>
2024-06-03 07:10:37 +0200

    Does the account exist?

--- server.js
+++ server.js
@@ -15,16 +15,30 @@ app.get('/', async (req, res) => {
   res.render('pages/index');  // Dit moet correct verwijzen naar 'views/pages/index.ejs'
 });
 
-app.post('/login', async (req, res) =>{
-  const data = {
-    username: req.body.usernameLogin,
-    password: req.body.passwordLogin
-  };
+app.post('/login', async (req, res) => {
+  const { usernameLogin, passwordLogin } = req.body;
 
-  await collection.insertMany([data]);
+  try {
+    const user = await collection.findOne({ username: usernameLogin });
+    
+    if (!user) {
+      // Gebruiker niet gevonden
+      return res.render('pages/index', { error: 'Gebruikersnaam bestaat niet' });
+    }
 
-  res.render('pages/feed');  // Dit moet correct verwijzen naar 'views/pages/feed.ejs'
+    if (user.password !== passwordLogin) {
+      // Wachtwoord klopt niet
+      return res.render('pages/index', { error: 'Wachtwoord is incorrect' });
+    }
+
+    // Succesvolle login
+    res.render('pages/feed');
+  } catch (error) {
+    console.error('Error tijdens het inloggen:', error);
+    res.status(500).render('pages/index', { error: 'Er is een fout opgetreden, probeer het opnieuw' });
+  }
 });
+
 
 app.get('/feed', async (req, res) =>{
   res.render('pages/feed.ejs')
